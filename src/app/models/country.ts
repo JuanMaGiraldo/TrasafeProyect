6df2460e893ebfff1887505c32908bd8a0bc38f9
JuanMaGiraldo <juanmagiraldo@hotmail.com>
2021-06-13 21:33:07 -0500

    Fixing styles

--- src/app/models/country.ts
+++ src/app/models/country.ts
@@ -1,79 +1,96 @@
 export class Country {
-    country: string;
-    departments: Department[];
+  country: string;
+  departments: Department[];
 
-    constructor(name, array) {
-        this.country = name;
-        this.departments = [];
+  constructor(name, array) {
+    this.country = name;
+    this.departments = [];
 
-        for (var dept of array) {
-            this.departments.push(new Department(dept["department"], dept["cities"]));
-        }
+    for (var dept of array) {
+      this.departments.push(new Department(dept["department"], dept["cities"]));
     }
+  }
 }
 
 export class Department {
-    department: string;
-    cities: City[];
-    constructor(name, array) {
-        this.department = name;
-        this.cities = [];
-        for (var city of array) {
-            this.cities.push(new City(city["city"], city["locations"]));
-        }
+  department: string;
+  cities: City[];
+  constructor(name, array) {
+    this.department = name;
+    this.cities = [];
+    for (var city of array) {
+      this.cities.push(new City(city["city"], city["locations"]));
     }
+  }
 }
 
 export class City {
-    city: string;
-    locations: Location[];
-    constructor(name, array) {
-        this.city = name;
-        this.locations = [];
-        for (var location of array) {
-            this.locations.push(new Location(location["location"], location["theftId"], location["terrorismId"], location["theftRating"], location["terrorismRating"], location["lat"], location["lng"]));
-        }
+  city: string;
+  locations: Location[];
+  constructor(name, array) {
+    this.city = name;
+    this.locations = [];
+    for (var location of array) {
+      this.locations.push(
+        new Location(
+          location["location"],
+          location["theftId"],
+          location["terrorismId"],
+          location["theftRating"],
+          location["terrorismRating"],
+          location["lat"],
+          location["lng"]
+        )
+      );
     }
+  }
 }
 
-
 export class Location {
-    location: string;
-    theftId: string;
-    terrorismId: string;
-    theftRating: string;
-    terrorismRating: string;
-    lat: string = "";
-    lng: string = "";
+  location: string;
+  theftId: string;
+  terrorismId: string;
+  theftRating: string;
+  terrorismRating: string;
+  lat: string = "";
+  lng: string = "";
 
-    constructor(location?, theftId?, terrorismId?, theftRating?, terrorismRating?, lat?, lng?) {
-        if (location != "") {
-            this.location = location;
-            this.theftId = theftId;
-            this.terrorismId = terrorismId;
-            this.theftRating = theftRating;
-            this.terrorismRating = terrorismRating;
-            if (lat && lng) {
-                this.lat = lat;
-                this.lng = lng;
-            }
-        }
-    }
-
-    public isLatLongDefined(): boolean {
-        return this.lat != "" && this.lng != "";
-    }
-
-    public getLatLng() {
-        return { lat: Number(this.lat), lng: Number(this.lng) };
-    }
-
-    public googleCanFindLocation() {
-        return this.lat != "none" && this.lng != "none";
-    }
-
-    public setLatLng(lat, lng) {
+  constructor(
+    location?,
+    theftId?,
+    terrorismId?,
+    theftRating?,
+    terrorismRating?,
+    lat?,
+    lng?
+  ) {
+    if (location != "") {
+      this.location = location;
+      this.theftId = theftId;
+      this.terrorismId = terrorismId;
+      this.theftRating = theftRating;
+      this.terrorismRating = terrorismRating;
+      if (lat && lng) {
         this.lat = lat;
         this.lng = lng;
+      }
     }
+  }
+
+  public isLatLongDefined(): boolean {
+    return this.lat != "" && this.lng != "";
+  }
+
+  public getLatLng() {
+    return { lat: Number(this.lat), lng: Number(this.lng) };
+  }
+
+  public googleCanFindLocation() {
+    return this.lat != "none" && this.lng != "none";
+  }
+
+  public setLatLng(lat, lng) {
+    this.lat = lat;
+    this.lng = lng;
+  }
 }
--- src/app/pages/load-info/load-info.page.ts
+++ src/app/pages/load-info/load-info.page.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { Country, Department, City, Location } from '../../models/country';
-import { Storage } from '@ionic/storage';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Country, Department, City, Location } from "../../models/country";
+import { Storage } from "@ionic/storage";
+import { NavController } from "@ionic/angular";
 @Component({
-  selector: 'app-load-info',
-  templateUrl: './load-info.page.html',
-  styleUrls: ['./load-info.page.scss'],
+  selector: "app-load-info",
+  templateUrl: "./load-info.page.html",
+  styleUrls: ["./load-info.page.scss"],
 })
 export class LoadInfoPage implements OnInit {
-
-  private state: string = ""
-  private console: string = ""
+  private state: string = "";
+  private console: string = "";
 
   constructor(
     private af: AngularFireDatabase,
     private storage: Storage,
     private navCtrl: NavController
   ) {
-    this.state = "Verificando datos..."
+    this.state = "Verificando datos...";
   }
 
   ngOnInit() {
@@ -28,7 +27,6 @@ export class LoadInfoPage implements OnInit {
   verifyInfo() {
     var info_loaded = false;
     this.storage.get("locationsArray").then((res) => {
-
       if (res != null && res != "") {
         var countryInfo: Country = res;
         if (countryInfo != null && countryInfo.departments.length == 32) {
@@ -44,25 +42,31 @@ export class LoadInfoPage implements OnInit {
   }
 
   async getLocationsFirebase() {
-    this.state = "Descargando los datos de la aplicación..."
-    this.af.list("/").valueChanges().subscribe((val) => {
-      var countryInfo: Country = new Country(val[0], val[1]);
-      this.saveDatabase(countryInfo);
-    },
-      (error) => this.state = "Error descargando los datos, verifique la conexión a Internet.");
+    this.state = "Descargando los datos de la aplicación...";
+    this.af
+      .list("/")
+      .valueChanges()
+      .subscribe(
+        (val) => {
+          var countryInfo: Country = new Country(val[0], val[1]);
+          this.saveDatabase(countryInfo);
+        },
+        (error) =>
+          (this.state =
+            "Error descargando los datos, verifique la conexión a Internet.")
+      );
   }
 
   async saveDatabase(countryInfo: Country) {
     if (countryInfo != null && countryInfo.departments.length == 32) {
       await this.storage.set("locationsArray", countryInfo);
       this.goToMap();
-    }
-    else {
-      this.state = "Error descargando los datos, vuelve a abrir la aplicación."
+    } else {
+      this.state = "Error descargando los datos, vuelve a abrir la aplicación.";
     }
   }
 
   goToMap() {
-    this.navCtrl.navigateRoot('/home');
+    this.navCtrl.navigateRoot("/home");
   }
 }
